1fc37d5ba7e5b0cd67db84473a9d98cff29ed9df
elahemortazavi <emor5680@gmail.com>
2023-02-08 08:55:01 +0000

    programmer completed

--- challenges/programmer-humour/script.js
+++ challenges/programmer-humour/script.js
@@ -16,15 +16,39 @@
 //   let image = document.createElement("img");
 //   mainDiv.appendChild(image);
 
-   function fetchData(){
+//    function fetchData(){
+//     fetch("https://xkcd.now.sh/?comic=latest")
+//     .then((response) => response.json())
+//     .then((data) => {
+//         let mainDiv = document.getElementById("container");
+//         let image = document.createElement("img");
+//         image.src = data.img;
+//         mainDiv.appendChild(image);
+
+//     })
+
+//    }
+
+
+
+
+let uList = document.querySelector("ul");
+let button = document.getElementById("btn");
+
+
+function fetchData() {
     fetch("https://xkcd.now.sh/?comic=latest")
-    .then((response) => response.json())
-    .then((data) => {
-        let mainDiv = document.getElementById("container");
+      .then((response) => response.json())
+      .then((data) => {
+        let list = document.createElement("li");     
         let image = document.createElement("img");
         image.src = data.img;
-        mainDiv.appendChild(image);
+        uList.appendChild(list);
+        list.appendChild(image);
 
-    })
+      })
+};
 
-   }
+button.addEventListener (("click"), () => {
+ fetchData();
+})
